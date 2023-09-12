93693d31cf105a8eb57c8494bb50d76d5e4e1a45
savbeM <mark.selihov2458@gmail.com>
2023-09-12 21:44:30 +0300

    added todayWeatherReducer.ts

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -2,11 +2,13 @@ import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {userInfoReducer} from "./userInfoReducer";
 import {currentWeatherReducer} from "./currentWeatherReducer";
 import thunk from "redux-thunk";
+import {todayWeatherReducer} from "./todayWeatherReducer";
 
 
 export const store = configureStore({
         reducer: combineReducers({
             userInfo: userInfoReducer,
+            todayWeather: todayWeatherReducer,
             currentWeather: currentWeatherReducer
         }),
         middleware: [thunk],
