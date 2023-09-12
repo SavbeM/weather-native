85d3cf5b8ebf2158cfb2950147b86e199d2cb608
savbeM <mark.selihov2458@gmail.com>
2023-09-12 21:36:15 +0300

    currentData changed to todayData request

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosRequestConfig, AxiosResponse} from "axios";
-import {CurrentWeather} from "../types/currentWeatherTypes";
 import {UserLocationData} from "../types/userInfoTypes";
+import {WeatherData} from "../types/WeatherTypes";
 
 
 const axiosConfig: AxiosRequestConfig = {
@@ -14,12 +14,10 @@ const axiosConfig: AxiosRequestConfig = {
 
 const reqInstance = axios.create(axiosConfig);
 
-export const getCurrentWeather = async (long: number, lat: number) =>
-    reqInstance.get(`?latitude=${lat}&longitude=${long}&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,cloudcover,visibility&forecast_days=1`)
-        .then((res: AxiosResponse<CurrentWeather>) => res.data)
-
 export const getGeolocationData = async (long: number, lat: number) =>
     axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=json`)
         .then((resp: AxiosResponse<UserLocationData>) => resp.data)
 
-
+export const getTodayWeather = async (long: number, lat: number ) =>
+    reqInstance.get(`?latitude=${lat}&longitude=${long}&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,cloudcover,windspeed_10m&daily=sunrise,sunset&timezone=auto&forecast_days=1`)
+    .then((resp: AxiosResponse<WeatherData>) => resp.data)
