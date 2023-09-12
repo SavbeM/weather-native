d02ef941c81e2c026f086ac562200be6b9e88fc2
savbeM <mark.selihov2458@gmail.com>
2023-09-12 21:43:39 +0300

    asynchronous logic are replaced, current data gets from hourly data in todayWeather state

--- src/store/currentWeatherReducer.ts
+++ src/store/currentWeatherReducer.ts
@@ -1,64 +1,39 @@
 import {
-    CurrentWeather,
-    CurrentWeatherState, GetCurrentWeatherThunkType, SetCurrentWeatherActionCreatorType,
+
+    CurrentWeatherState, SetCurrentWeatherActionCreatorType,
 
 } from "../types/currentWeatherTypes";
-import {Dispatch} from "redux";
-import {getCurrentWeather} from "../api/requests";
-import {IsPendingActionCreatorType, SetErrorActionCreatorType} from "../types/globalTypes";
 
+import {HourlyData} from "../types/WeatherTypes";
 
-export const IS_PENDING = "IS_PENDING"
-export const SET_ERROR = "SET_ERROR"
 export const SET_CURRENT_WEATHER = "SET_CURRENT_WEATHER"
 
 const initialState: CurrentWeatherState = {
     currentWeather: undefined,
-    isPending: false,
-    error: null
-}
-
-const isPendingActionCreator = (pendingStatus: boolean): IsPendingActionCreatorType => {
-    return {type: IS_PENDING, pendingStatus}
-}
-
-const setErrorActionCreator = (error: Error): SetErrorActionCreatorType => {
-    return {type: SET_ERROR, error}
 }
 
 
-const setCurrentWeatherActionCreator = (currentWeather: CurrentWeather): SetCurrentWeatherActionCreatorType => {
-    return {type: SET_CURRENT_WEATHER, currentWeather}
+export const setCurrentWeatherActionCreator = (hourlyData: HourlyData): SetCurrentWeatherActionCreatorType => {
+    return {type: SET_CURRENT_WEATHER, hourlyData}
 }
 
-export const getCurrentWeatherThunk = (long: number, lat: number): GetCurrentWeatherThunkType => {
-    return async (dispatch: Dispatch<CurrentWeatherActions>) => {
-        dispatch(isPendingActionCreator(true))
-        try {
-            const currentWeather = await getCurrentWeather(long, lat)
-            dispatch(setCurrentWeatherActionCreator(currentWeather))
-        } catch (error: any) {
-            dispatch(setErrorActionCreator(new Error(error)))
-        } finally {
-            dispatch(isPendingActionCreator(false))
-        }
-    }
-}
+
 
 export type CurrentWeatherActions =
     SetCurrentWeatherActionCreatorType
-    | SetErrorActionCreatorType
-    | IsPendingActionCreatorType
-
 
 export const currentWeatherReducer = (state = initialState, action: CurrentWeatherActions): CurrentWeatherState => {
     switch (action.type) {
         case "SET_CURRENT_WEATHER":
-            return {...state, currentWeather: {...action.currentWeather}}
-        case "IS_PENDING":
-            return {...state, isPending: action.pendingStatus}
-        case "SET_ERROR":
-            return {...state, error: action.error}
+            const currentTime =  new Date().getHours()
+            return {...state, currentWeather: {
+                temperature_2m: action.hourlyData.temperature_2m[currentTime],
+                    apparent_temperature: action.hourlyData.apparent_temperature[currentTime],
+                    precipitation_probability: action.hourlyData.precipitation_probability[currentTime],
+                    precipitation: action.hourlyData.precipitation[currentTime],
+                    cloudcover: action.hourlyData.cloudcover[currentTime],
+                    windspeed_10m: action.hourlyData.windspeed_10m[currentTime],
+                }}
         default:
             return state
     }
--- src/types/currentWeatherTypes.ts
+++ src/types/currentWeatherTypes.ts
@@ -1,48 +1,23 @@
-import {CurrentWeatherActions,  SET_CURRENT_WEATHER} from "../store/currentWeatherReducer";
-import {ThunkAction} from "redux-thunk";
-import {RootState} from "../store/store";
+import {SET_CURRENT_WEATHER} from "../store/currentWeatherReducer";
+import {HourlyData} from "./WeatherTypes";
 
 
-export interface HourlyUnits  {
-    time: string,
-    temperature_2m: "°C",
-    apparent_temperature: "°C",
-    precipitation_probability: "%",
-    precipitation: "mm",
-    cloudcover: "%",
-    visibility: "m",
-}
 
-export interface HourlyVar {
-    time: Array<Date>,
-    temperature_2m: Array<number>,
-    apparent_temperature: Array<number>,
-    precipitation_probability: Array<number>,
-    visibility: Array<number>,
-    precipitation: Array<number>,
-    cloudcover: Array<number>
 
-}
 
-export interface CurrentWeather {
-    latitude: number,
-    longitude: number,
-    generationtime_ms: number,
-    utc_offset_seconds: number,
-    timezone: string,
-    timezone_abbreviation: string,
-    elevation: string,
-    hourly_units: HourlyUnits,
-    hourly: HourlyVar
-}
 
 export interface CurrentWeatherState{
     currentWeather: CurrentWeather | undefined,
-    isPending: boolean,
-    error: Error | null
 }
 
+interface CurrentWeather {
+    temperature_2m: number;
+    apparent_temperature: number;
+    precipitation_probability: number;
+    precipitation: number;
+    cloudcover: number;
+    windspeed_10m: number;
+}
 
-export type GetCurrentWeatherThunkType = ThunkAction<void, RootState, unknown,  CurrentWeatherActions>
+export type SetCurrentWeatherActionCreatorType = {type: typeof SET_CURRENT_WEATHER, hourlyData: HourlyData}
 
-export type SetCurrentWeatherActionCreatorType = {type: typeof SET_CURRENT_WEATHER, currentWeather: CurrentWeather}
